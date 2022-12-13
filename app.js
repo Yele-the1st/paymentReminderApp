da440559858a66baaa03d85a5c1948c0edef0533
Olabode Omoyele <yele.olabode@gmail.com>
2022-12-13 01:54:35 +0100

    connect to DB

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const adminRoutes = require("./routes/admin");
 
 const app = express();
 
-app.listen(5000);
+app.use(express.json());
+
+app.use("/admin", adminRoutes);
+
+//connecting to DB & Starting Server
+const PORT = process.env.PORT || 1000;
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    app.listen(PORT);
+    console.log("Connection to DB has been established successfully.");
+    console.log(`server up on ${PORT}`);
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database:", err);
+  });
